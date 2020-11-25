eac38ae425da47aea07043efa2275fa65b2e0d0e
tobao17 <phamtobao99@gmail.com>
2020-11-25 23:29:05 +0700

    fix core file

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ var bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 mongoose
-  .connect(process.env.Mongo_URL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  })
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error(`Connection failed...`, err));
+	.connect(process.env.Mongo_URL, {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+	})
+	.then(() => console.log("Connected to MongoDB..."))
+	.catch((err) => console.error(`Connection failed...`, err));
 mongoose.set("useFindAndModify", false);
 
 //
 //
 //
 //
+app.use(cors());
 const bookRouter = require("./router/book.router");
 const categoriesRouter = require("./router/categories.router");
 const userRouter = require("./router/user.router");
@@ -28,7 +29,6 @@ const orderRouter = require("./router/order.router");
 //
 //
 
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -39,8 +39,8 @@ app.use("/categori", categoriesRouter);
 app.use("/user", userRouter);
 app.use("/cart", cartRouter);
 app.use("/order", orderRouter);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 // listen for requests :)
 app.listen(port, () => {
-  console.log("Server listening on port " + port);
+	console.log("Server listening on port " + port);
 });
