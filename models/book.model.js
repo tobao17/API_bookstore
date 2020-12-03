6868be53d988db54dd4befeb7b9a10e6722aee1e
tobao17 <phamtobao99@gmail.com>
2020-12-03 21:43:29 +0700

    add time timestamp

--- models/book.model.js
+++ models/book.model.js
@@ -1,18 +1,21 @@
 const mongoose = require("mongoose");
-var bookSchema = new mongoose.Schema({
-	title: String,
-	author: String,
-	description: String,
-	price: Number,
-	status: Number,
-	quantity: Number,
+var bookSchema = new mongoose.Schema(
+	{
+		title: String,
+		author: String,
+		description: String,
+		price: Number,
+		status: Number,
+		quantity: Number,
 
-	//seller: { type: mongoose.Schema.Types.ObjectId }, //, ref: "User"
-	category: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "categories",
+		//seller: { type: mongoose.Schema.Types.ObjectId }, //, ref: "User"
+		category: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "categories",
+		},
+		images: String,
 	},
-	images: String,
-});
+	{ timestamps: true }
+);
 var Book = mongoose.model("books", bookSchema);
 module.exports = Book;
