c161cf0aa459a94e81a16de0975d59e7327f01d4
tobao17 <phamtobao99@gmail.com>
2020-12-15 20:38:22 +0700

    terminate token -> test fronent

--- router/order.router.js
+++ router/order.router.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/verifiedToken.middleware");
 const orderController = require("../controller/order.controller");
-router.post("/add", auth.verified, orderController.add);
-router.get("/delete/:id", auth.verified, orderController.deleteOrder);
+router.post("/add", orderController.add); // all product =>  loai bo de test  // auth.verified
+router.get("/delete/:id", orderController.deleteOrder);
 
-router.get("/myorder", auth.verified, orderController.checkorder);
-router.get("/", auth.verified, orderController.index);
+router.get("/myorder", orderController.checkorder);
+router.get("/", orderController.index);
 module.exports = router;
