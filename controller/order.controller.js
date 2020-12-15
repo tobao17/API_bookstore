98edbce409d60b954bcbcbde9e8192032b586d8a
tobao17 <phamtobao99@gmail.com>
2020-12-15 21:34:41 +0700

    fix order for api

--- controller/order.controller.js
+++ controller/order.controller.js
@@ -5,8 +5,12 @@ module.exports.index = async (req, res) => {
 	try {
 		const orderlist = await Order.find({})
 			.populate("products.book", "-description -isDelete -quantity")
+			.populate(
+				"user",
+				"-role -wrongLoginCount -status -wallet -password -cart -createdAt -updatedAt -address"
+			)
 			.sort({ status: 1 });
-		res.status(200).json(orderlist);
+		res.status(200).json({ msg: "success!", data: orderlist });
 	} catch (error) {
 		res.status(400).json(error);
 	}
@@ -14,8 +18,13 @@ module.exports.index = async (req, res) => {
 module.exports.checkorder = async (req, res) => {
 	const userId = req.token.user.id;
 	try {
-		const myOrder = await Order.find({ user: userId, status: !2 });
-		return res.status(200).json({ msg: ` success!`, order: myOrder });
+		const myOrder = await Order.find({ user: userId, status: !2 })
+			.populate("products.book", "-description -isDelete -quantity")
+			.populate(
+				"user",
+				"-role -wrongLoginCount -status -wallet -password -cart -createdAt -updatedAt -address"
+			);
+		return res.status(200).json({ msg: ` success!`, data: myOrder });
 	} catch (error) {
 		return res
 			.status(400)
@@ -30,7 +39,7 @@ module.exports.add = async (req, res) => {
 		console.log(req.body);
 		const order = await Order.create(req.body);
 		await User.findOneAndUpdate({ _id: req.body.user }, { cart: [] });
-		return res.status(201).json({ msd: "add order success!", order: order });
+		return res.status(201).json({ msg: "add order success!", data: order });
 	} catch (error) {
 		return res.status(400).json({
 			msd: `your request could not be processed! +${error}`,
--- router/order.router.js
+++ router/order.router.js
@@ -4,7 +4,6 @@ const auth = require("../middleware/verifiedToken.middleware");
 const orderController = require("../controller/order.controller");
 router.post("/add", orderController.add); // all product =>  loai bo de test  // auth.verified
 router.get("/delete/:id", orderController.deleteOrder);
-
 router.get("/myorder", orderController.checkorder);
 router.get("/", orderController.index);
 module.exports = router;
