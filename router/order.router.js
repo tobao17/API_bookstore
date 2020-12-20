383a496334d265a8e3cafc195f01d962fe9adcde
tobao17 <phamtobao99@gmail.com>
2020-12-21 03:03:15 +0700

    fix order

--- router/order.router.js
+++ router/order.router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/verifiedToken.middleware");
 const orderController = require("../controller/order.controller");
-router.post("/add", orderController.add); // all product =>  loai bo de test  // auth.verified
+router.post("/add", auth.verified, orderController.add); // all product =>  loai bo de test  // auth.verified
 router.post("/update", orderController.update);
 router.get("/myorder", auth.verified, orderController.checkorder);
 router.get("/", orderController.index);
